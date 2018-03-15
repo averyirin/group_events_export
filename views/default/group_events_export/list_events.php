d05239070c1dbe88abad638f0e4c26513c363884
Irin <mpg.students+borden@gmail.com>
2018-03-15 10:15:46 -0400

    textarea csv

--- views/default/group_events_export/list_events.php
+++ views/default/group_events_export/list_events.php
@@ -4,9 +4,7 @@ $listEvents = $vars['listEvents'];
 <div>
 
     <div  class="col col-xs-12">
-      <?php
-        echo var_dump($listEvents);
-      ?>
+      <textarea name="csv" rows="8" cols="80"><?php echo $listEvents;?></textarea>
     </div>
 
 
