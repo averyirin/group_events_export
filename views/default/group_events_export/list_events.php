9c3e8215eb502209c715498e46009c6231a7024c
Irin <mpg.students+borden@gmail.com>
2018-03-15 11:34:25 -0400

    export btn

--- views/default/group_events_export/list_events.php
+++ views/default/group_events_export/list_events.php
@@ -5,12 +5,14 @@ $listEvents = $vars['listEvents'];
 
     <div  class="col col-xs-12">
       <textarea name="csv" rows="8" cols="80"><?php echo $listEvents;?></textarea>
+  <div>
+  <?php
+   elgg_view("output/url",
+   array("is_action" => true,
+   "href" => "action/group_events_export/group_events_export/csv?groupGuid=1742051",
+   "text" => "Export to CSV"));
+  ?>
+</div>
     </div>
-<?php
- elgg_view("output/url",
- array("is_action" => true,
- "href" => "action/group_events_export/group_events_export/csv?groupGuid=1742051",
- "text" => "Export to CSV"));
-?>
 
 </div>
