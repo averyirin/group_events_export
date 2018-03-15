67f9d3b6bd822802f3de2c0c9a9df1093a2c31bc
Irin <mpg.students+borden@gmail.com>
2018-03-15 15:07:52 -0400

    cleaned up spreadsheet

--- actions/group_events_export/csv.php
+++ actions/group_events_export/csv.php
@@ -3,7 +3,7 @@
 $groupGuid = (int) get_input("groupGuid");
 $csvExportString = generate_group_events_spreadsheet($groupGuid);
 header("Content-type: application/vnd.ms-excel");
-header("Content-Disposition: Attachment; filename=export.csv");
+header("Content-Disposition: Attachment; filename=export.xls");
 header('Pragma: public');
 echo $csvExportString;
 exit;
