157a9e8f530005520c1230c0eefe3dd98cf5b466
Irin <mpg.students+borden@gmail.com>
2018-03-15 11:49:30 -0400

    export btn

--- actions/group_events_export/csv.php
+++ actions/group_events_export/csv.php
@@ -1,9 +1,16 @@
 <?php
+
+
 $groupGuid = (int) get_input("groupGuid");
+
+$csvExportString = generate_group_events_spreadsheet($groupGuid);
+
+
+
+
 
 header("Content-type: application/vnd.ms-excel");
 header("Content-Disposition: Attachment; filename=export.csv");
 header('Pragma: public');
-
-echo generate_group_events_spreadsheet($groupGuid);
+echo $csvExportString;
 exit;
