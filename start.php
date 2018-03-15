64e7d99a100ea3ae1913137c21fb949466f6e549
Irin <mpg.students+borden@gmail.com>
2018-03-15 15:22:49 -0400

    cleaned up spreadsheet

--- start.php
+++ start.php
@@ -14,6 +14,8 @@ elgg_register_event_handler('init', 'system', 'group_events_export_init');
 //when the plugin is active
 function group_events_export_init()
 {
+
+
     //page setup handler to add the group export events button
     elgg_register_event_handler("pagesetup", "system", "group_events_export_pagesetup", 550);
     //register group_events_export library
@@ -28,15 +30,20 @@ function group_events_export_pagesetup()
 {
     //if we can edit the group
     $page_owner = elgg_get_page_owner_entity();
+    $who_create_group_events = elgg_get_plugin_setting('who_create_group_events', 'event_manager'); // group_admin, members
+  if((($who_create_group_events == "group_admin") && $page_owner->canEdit()) || (($who_create_group_events == "members") && $page_owner->isMember(elgg_get_logged_in_user_guid()))){
     //add the group statistics button to the group events menu
     //! Does this need security?
     if(elgg_in_context("events")&& ($page_owner instanceof ElggGroup)) {
       elgg_register_menu_item('title', array(
-								'name' => "export",
-								'href' => "action/group_events_export/csv?groupGuid=".$page_owner->getGuid(),
-								'text' => "Export Group Events",
-								'link_class' => 'elgg-button elgg-button-action',
+                'name' => "export",
+                'href' => "action/group_events_export/csv?groupGuid=".$page_owner->getGuid(),
+                'text' => "Export Group Events",
+                'link_class' => 'elgg-button elgg-button-action',
                 'is_action' => true
-								));
+                ));
     }
+
+  }
+
 }
