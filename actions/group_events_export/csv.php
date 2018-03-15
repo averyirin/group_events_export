21e3cd41cfee5718ee445954802346bb5cb325ae
Irin <mpg.students+borden@gmail.com>
2018-03-15 13:09:33 -0400

    cleaned

--- actions/group_events_export/csv.php
+++ actions/group_events_export/csv.php
@@ -1,13 +1,7 @@
 <?php
 
 $groupGuid = (int) get_input("groupGuid");
-
 $csvExportString = generate_group_events_spreadsheet($groupGuid);
-
-
-
-
-
 header("Content-type: application/vnd.ms-excel");
 header("Content-Disposition: Attachment; filename=export.csv");
 header('Pragma: public');
--- start.php
+++ start.php
@@ -3,11 +3,10 @@
  * Author: Irin Avery
  * Desc: Start file for group_statistics plugin
  */
-//redirect action handler
+//group events export action handler
 $action_path = elgg_get_plugins_path() . 'group_events_export/actions/group_events_export';
-//register redirection and forms
+//register export action
 elgg_register_action("group_events_export/csv", $action_path . "/csv.php");
-elgg_register_action("subtypeForm", $action_path . "/subtypeForm.php");
 //register plugin init
 elgg_register_event_handler('init', 'system', 'group_events_export_init');
 
@@ -17,17 +16,10 @@ function group_events_export_init()
 {
     //page setup handler to add the group export events button
     elgg_register_event_handler("pagesetup", "system", "group_events_export_pagesetup", 550);
-    //register group_statistics library
+    //register group_events_export library
     elgg_register_library('group_events_export:lib', elgg_get_plugins_path() . 'group_events_export/lib/group_events_export.php');
-    //register page handler
     //load library
     elgg_load_library('group_events_export:lib');
-
-
-    //if we are in the group statistics register libraries
-    if (get_context() == 'group_events_export') {
-        elgg_register_page_handler('group_events_export', 'group_events_export_page_handler');
-    }
 }
 
 
@@ -36,7 +28,8 @@ function group_events_export_pagesetup()
 {
     //if we can edit the group
     $page_owner = elgg_get_page_owner_entity();
-    //add the group statistics button to the group admin menu
+    //add the group statistics button to the group events menu
+    //! Does this need security?
     if(elgg_in_context("events")&& ($page_owner instanceof ElggGroup)) {
       elgg_register_menu_item('title', array(
 								'name' => "export",
@@ -46,38 +39,4 @@ function group_events_export_pagesetup()
                 'is_action' => true
 								));
     }
-}
-
-
-/**
- *Directs group admin to their statistics page
- * group_statistics/dashboard/Group ID/Item ID
- *
- * @param array $page
- * @return bool
- */
-function group_events_export_page_handler($page)
-{
-
-    //set page, groupID and itemID
-    $pageNav = $page[0];
-
-    $groupID = $page[1];
-    //Looking for
-    //group_statistics/dashboard/GroupID/ItemId
-    switch ($pageNav) {
-        case "export_events":
-            //if we have itemID, go to item view with back btn
-            if ($groupID!= NULL) {
-                $params = get_events_from_group($groupID);
-                $body = elgg_view_layout('admin', $params);
-            }
-            break;
-        default:
-            return false;
-    }
-
-    //show the created page
-    echo elgg_view_page($params['title'], $body);
-    return true;
 }
