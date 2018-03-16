410c1dbe0937c2fdb0318a8604e2dd99637aafc3
Irin <mpg.students+borden@gmail.com>
2018-03-16 13:35:28 -0400

    spreadsheet format

--- lib/group_events_export.php
+++ lib/group_events_export.php
@@ -116,18 +116,12 @@ function group_events_export_sheet($event){
 
   $worksheetXml .=  '<Row>
   <Cell><Data ss:Type="String">Molly</Data></Cell>
-  <Cell ss:Index="3"><Data
+  <Cell><Data ss:Type="String">Polly</Data></Cell>
+  <Cell><Data
   ss:Type="String">Katzen</Data></Cell>
   <Cell ss:StyleID="s21" ss:HRef="mailto:molly@katzen.com">
   <Data ss:Type="String">molly@katzen.com</Data></Cell>
-  </Row>
-  <Row>
-<Cell><Data ss:Type="String">Molly</Data></Cell>
-<Cell ss:Index="3"><Data
-ss:Type="String">Katzen</Data></Cell>
-<Cell ss:StyleID="s21" ss:HRef="mailto:molly@katzen.com">
-<Data ss:Type="String">molly@katzen.com</Data></Cell>
-</Row>';
+  </Row>';
 
   $headerXml = '
    <Worksheet ss:Name="'.$event->title.'">
