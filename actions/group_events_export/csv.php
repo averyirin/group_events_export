1b11be56709445cd5afaaf347ff7fb78b52a5e32
Irin <mpg.students+borden@gmail.com>
2018-03-16 09:12:07 -0400

    spreadsheet format

--- actions/group_events_export/csv.php
+++ actions/group_events_export/csv.php
@@ -1,7 +1,7 @@
 <?php
 
 $groupGuid = (int) get_input("groupGuid");
-/*
+
 $csvExportString = generate_group_events_spreadsheet($groupGuid);
 header("Content-type: text/csv");
 header("Content-Disposition: Attachment; filename=export.csv");
@@ -9,10 +9,11 @@ header('Pragma: public');
 echo $csvExportString;
 exit;
 
-*/
+/*
 $csvExportString = getGroupEventXML();
 header("Content-type: text/xml");
 header("Content-Disposition: Attachment; filename=export.xml");
 header('Pragma: public');
 echo $csvExportString;
 exit;
+*/
