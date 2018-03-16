bbe7a3aacc6c8baa79c455842078b10759d457ca
Irin <mpg.students+borden@gmail.com>
2018-03-16 08:44:34 -0400

    spreadsheet format

--- actions/group_events_export/csv.php
+++ actions/group_events_export/csv.php
@@ -1,9 +1,18 @@
 <?php
 
 $groupGuid = (int) get_input("groupGuid");
+/*
 $csvExportString = generate_group_events_spreadsheet($groupGuid);
 header("Content-type: text/csv");
 header("Content-Disposition: Attachment; filename=export.csv");
+header('Pragma: public');
+echo $csvExportString;
+exit;
+
+*/
+$csvExportString = getGroupEventXML();
+header("Content-type: text/xml");
+header("Content-Disposition: Attachment; filename=export.xml");
 header('Pragma: public');
 echo $csvExportString;
 exit;
--- lib/group_events_export.php
+++ lib/group_events_export.php
@@ -118,7 +118,101 @@ function group_events_export_comma($event) {
 		elgg_set_ignore_access($old_ia);
 		return $dataString;
 	}
-
+function getGroupEventXML(){
+  $xml = '<?xml version="1.0"?>
+<?mso-application progid="Excel.Sheet"?>
+<Workbook xmlns="urn:schemas-microsoft-com:office:spreadsheet"
+ xmlns:o="urn:schemas-microsoft-com:office:office"
+ xmlns:x="urn:schemas-microsoft-com:office:excel"
+ xmlns:ss="urn:schemas-microsoft-com:office:spreadsheet"
+ xmlns:html="http://www.w3.org/TR/REC-html40">
+ <DocumentProperties xmlns="urn:schemas-microsoft-com:office:office">
+  <Author>Jack Herrington</Author>
+  <LastAuthor>Jack Herrington</LastAuthor>
+  <Created>2005-08-02T04:06:26Z</Created>
+  <LastSaved>2005-08-02T04:30:11Z</LastSaved>
+  <Company>My Software Company, Inc.</Company>
+  <Version>11.6360</Version>
+  </DocumentProperties>
+  <ExcelWorkbook xmlns="urn:schemas-microsoft-com:office:excel">
+  <WindowHeight>8535</WindowHeight>
+  <WindowWidth>12345</WindowWidth>
+  <WindowTopX>480</WindowTopX>
+  <WindowTopY>90</WindowTopY>
+  <ProtectStructure>False</ProtectStructure>
+  <ProtectWindows>False</ProtectWindows>
+  </ExcelWorkbook>
+  <Styles>
+  <Style ss:ID="Default" ss:Name="Normal">
+  <Alignment ss:Vertical="Bottom"/>
+  <Borders/>
+  <Font/>
+  <Interior/>
+  <NumberFormat/>
+  <Protection/>
+  </Style>
+  <Style ss:ID="s21" ss:Name="Hyperlink">
+  <Font ss:Color="#0000FF" ss:Underline="Single"/>
+  </Style>
+  <Style ss:ID="s23">
+  <Font x:Family="Swiss" ss:Bold="1"/>
+  </Style>
+  </Styles>
+  <Worksheet ss:Name="Sheet1">
+  <Table ss:ExpandedColumnCount="4"
+  ss:ExpandedRowCount="5" x:FullColumns="1"
+  x:FullRows="1">
+  <Column ss:Index="4" ss:AutoFitWidth="0" ss:Width="154.5"/>
+  <Row ss:StyleID="s23">
+  <Cell><Data ss:Type="String">First</Data></Cell>
+  <Cell><Data ss:Type="String">Middle</Data></Cell>
+  <Cell><Data ss:Type="String">Last</Data></Cell>
+  <Cell><Data ss:Type="String">Email</Data></Cell>
+  </Row>
+  <Row>
+  <Cell><Data ss:Type="String">Molly</Data></Cell>
+  <Cell ss:Index="3"><Data
+  ss:Type="String">Katzen</Data></Cell>
+  <Cell ss:StyleID="s21" ss:HRef="mailto:molly@katzen.com">
+  <Data ss:Type="String">molly@katzen.com</Data></Cell>
+  </Row>
+  ...
+  </Table>
+  <WorksheetOptions
+  xmlns="urn:schemas-microsoft-com:office:excel">
+  <Print>
+  <ValidPrinterInfo/>
+  <HorizontalResolution>300</HorizontalResolution>
+  <VerticalResolution>300</VerticalResolution>
+  </Print>
+  <Selected/>
+  <Panes>
+  <Pane>
+  <Number>3</Number>
+  <ActiveRow>5</ActiveRow>
+  </Pane>
+  </Panes>
+  <ProtectObjects>False</ProtectObjects>
+  <ProtectScenarios>False</ProtectScenarios>
+  </WorksheetOptions>
+  </Worksheet>
+  <Worksheet ss:Name="Sheet2">
+  <WorksheetOptions
+  xmlns="urn:schemas-microsoft-com:office:excel">
+  <ProtectObjects>False</ProtectObjects>
+  <ProtectScenarios>False</ProtectScenarios>
+  </WorksheetOptions>
+  </Worksheet>
+  <Worksheet ss:Name="Sheet3">
+  <WorksheetOptions
+  xmlns="urn:schemas-microsoft-com:office:excel">
+  <ProtectObjects>False</ProtectObjects>
+  <ProtectScenarios>False</ProtectScenarios>
+  </WorksheetOptions>
+  </Worksheet>
+  </Workbook>';
+  return $xml;
+}
 
 
 
