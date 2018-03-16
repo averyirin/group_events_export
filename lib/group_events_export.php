92109fd2987d6fda4eb2a3882464e0ff8bb8d7ca
Irin <mpg.students+borden@gmail.com>
2018-03-16 08:58:20 -0400

    spreadsheet format

--- lib/group_events_export.php
+++ lib/group_events_export.php
@@ -120,97 +120,102 @@ function group_events_export_comma($event) {
 	}
 function getGroupEventXML(){
   $xml = '<?xml version="1.0"?>
-<?mso-application progid="Excel.Sheet"?>
-<Workbook xmlns="urn:schemas-microsoft-com:office:spreadsheet"
- xmlns:o="urn:schemas-microsoft-com:office:office"
- xmlns:x="urn:schemas-microsoft-com:office:excel"
- xmlns:ss="urn:schemas-microsoft-com:office:spreadsheet"
- xmlns:html="http://www.w3.org/TR/REC-html40">
- <DocumentProperties xmlns="urn:schemas-microsoft-com:office:office">
-  <Author>Irin Avery</Author>
-  <LastAuthor>Irin Avery</LastAuthor>
-  <Created>2005-08-02T04:06:26Z</Created>
-  <LastSaved>2005-08-02T04:30:11Z</LastSaved>
-  <Company>MPG LSC</Company>
-  <Version>11.6360</Version>
-  </DocumentProperties>
-  <ExcelWorkbook xmlns="urn:schemas-microsoft-com:office:excel">
-  <WindowHeight>8535</WindowHeight>
-  <WindowWidth>12345</WindowWidth>
-  <WindowTopX>480</WindowTopX>
-  <WindowTopY>90</WindowTopY>
-  <ProtectStructure>False</ProtectStructure>
-  <ProtectWindows>False</ProtectWindows>
-  </ExcelWorkbook>
-  <Styles>
-  <Style ss:ID="Default" ss:Name="Normal">
-  <Alignment ss:Vertical="Bottom"/>
-  <Borders/>
-  <Font/>
-  <Interior/>
-  <NumberFormat/>
-  <Protection/>
-  </Style>
-  <Style ss:ID="s21" ss:Name="Hyperlink">
-  <Font ss:Color="#0000FF" ss:Underline="Single"/>
-  </Style>
-  <Style ss:ID="s23">
-  <Font x:Family="Swiss" ss:Bold="1"/>
-  </Style>
-  </Styles>  
-  <Worksheet ss:Name="Sheet1">
-  <Table ss:ExpandedColumnCount="4"
-  ss:ExpandedRowCount="5" x:FullColumns="1"
-  x:FullRows="1">
-  <Column ss:Index="4" ss:AutoFitWidth="0" ss:Width="154.5"/>
-  <Row ss:StyleID="s23">
-  <Cell><Data ss:Type="String">First</Data></Cell>
-  <Cell><Data ss:Type="String">Middle</Data></Cell>
-  <Cell><Data ss:Type="String">Last</Data></Cell>
-  <Cell><Data ss:Type="String">Email</Data></Cell>
-  </Row>
-  <Row>
-  <Cell><Data ss:Type="String">Molly</Data></Cell>
-  <Cell ss:Index="3"><Data
-  ss:Type="String">Katzen</Data></Cell>
-  <Cell ss:StyleID="s21" ss:HRef="mailto:molly@katzen.com">
-  <Data ss:Type="String">molly@katzen.com</Data></Cell>
-  </Row>
-  ...
-  </Table>
-  <WorksheetOptions
-  xmlns="urn:schemas-microsoft-com:office:excel">
-  <Print>
-  <ValidPrinterInfo/>
-  <HorizontalResolution>300</HorizontalResolution>
-  <VerticalResolution>300</VerticalResolution>
-  </Print>
-  <Selected/>
-  <Panes>
-  <Pane>
-  <Number>3</Number>
-  <ActiveRow>5</ActiveRow>
-  </Pane>
-  </Panes>
-  <ProtectObjects>False</ProtectObjects>
-  <ProtectScenarios>False</ProtectScenarios>
-  </WorksheetOptions>
-  </Worksheet>
-  <Worksheet ss:Name="Sheet2">
-  <WorksheetOptions
-  xmlns="urn:schemas-microsoft-com:office:excel">
-  <ProtectObjects>False</ProtectObjects>
-  <ProtectScenarios>False</ProtectScenarios>
-  </WorksheetOptions>
-  </Worksheet>
-  <Worksheet ss:Name="Sheet3">
-  <WorksheetOptions
-  xmlns="urn:schemas-microsoft-com:office:excel">
-  <ProtectObjects>False</ProtectObjects>
-  <ProtectScenarios>False</ProtectScenarios>
-  </WorksheetOptions>
-  </Worksheet>
-  </Workbook>';
+  <?mso-application progid="Excel.Sheet"?>
+  <Workbook xmlns="urn:schemas-microsoft-com:office:spreadsheet"
+   xmlns:o="urn:schemas-microsoft-com:office:office"
+   xmlns:x="urn:schemas-microsoft-com:office:excel"
+   xmlns:ss="urn:schemas-microsoft-com:office:spreadsheet"
+   xmlns:html="http://www.w3.org/TR/REC-html40">
+   <DocumentProperties xmlns="urn:schemas-microsoft-com:office:office">
+    <Author>Irin Avery</Author>
+    <LastAuthor>Irin A</LastAuthor>
+    <Created>2005-08-02T04:06:26Z</Created>
+    <LastSaved>2005-08-02T04:30:11Z</LastSaved>
+    <Company>MPG LSC</Company>
+    <Version>14.00</Version>
+   </DocumentProperties>
+   <OfficeDocumentSettings xmlns="urn:schemas-microsoft-com:office:office">
+    <AllowPNG/>
+   </OfficeDocumentSettings>
+   <ExcelWorkbook xmlns="urn:schemas-microsoft-com:office:excel">
+    <WindowHeight>8535</WindowHeight>
+    <WindowWidth>12345</WindowWidth>
+    <WindowTopX>480</WindowTopX>
+    <WindowTopY>90</WindowTopY>
+    <ProtectStructure>False</ProtectStructure>
+    <ProtectWindows>False</ProtectWindows>
+   </ExcelWorkbook>
+   <Styles>
+    <Style ss:ID="Default" ss:Name="Normal">
+     <Alignment ss:Vertical="Bottom"/>
+     <Borders/>
+     <Font ss:FontName="Arial"/>
+     <Interior/>
+     <NumberFormat/>
+     <Protection/>
+    </Style>
+    <Style ss:ID="s62" ss:Name="Hyperlink">
+     <Font ss:FontName="Arial" ss:Color="#0000FF" ss:Underline="Single"/>
+    </Style>
+    <Style ss:ID="s63">
+     <Font ss:FontName="Arial" x:Family="Swiss" ss:Bold="1"/>
+    </Style>
+   </Styles>
+   <Worksheet ss:Name="Sheet1">
+    <Names>
+     <NamedRange ss:Name="_FilterDatabase" ss:RefersTo="=Sheet1!R1C1:R2C4"
+      ss:Hidden="1"/>
+    </Names>
+    <Table ss:ExpandedColumnCount="4" ss:ExpandedRowCount="2" x:FullColumns="1"
+     x:FullRows="1">
+     <Column ss:Index="4" ss:AutoFitWidth="0" ss:Width="154.5"/>
+     <Row ss:StyleID="s63">
+      <Cell><Data ss:Type="String">First</Data><NamedCell ss:Name="_FilterDatabase"/></Cell>
+      <Cell><Data ss:Type="String">Middle</Data><NamedCell ss:Name="_FilterDatabase"/></Cell>
+      <Cell><Data ss:Type="String">Last</Data><NamedCell ss:Name="_FilterDatabase"/></Cell>
+      <Cell><Data ss:Type="String">Email</Data><NamedCell ss:Name="_FilterDatabase"/></Cell>
+     </Row>
+     <Row>
+      <Cell><Data ss:Type="String">Molly</Data><NamedCell ss:Name="_FilterDatabase"/></Cell>
+      <Cell ss:Index="3"><Data ss:Type="String">Katzen</Data><NamedCell
+        ss:Name="_FilterDatabase"/></Cell>
+      <Cell ss:StyleID="s62" ss:HRef="mailto:molly@katzen.com"><Data ss:Type="String">molly@katzen.com</Data><NamedCell
+        ss:Name="_FilterDatabase"/></Cell>
+     </Row>
+    </Table>
+    <WorksheetOptions xmlns="urn:schemas-microsoft-com:office:excel">
+     <Print>
+      <ValidPrinterInfo/>
+      <HorizontalResolution>300</HorizontalResolution>
+      <VerticalResolution>300</VerticalResolution>
+     </Print>
+     <Selected/>
+     <ProtectObjects>False</ProtectObjects>
+     <ProtectScenarios>False</ProtectScenarios>
+    </WorksheetOptions>
+    <AutoFilter x:Range="R1C1:R2C4" xmlns="urn:schemas-microsoft-com:office:excel">
+    </AutoFilter>
+   </Worksheet>
+   <Worksheet ss:Name="Sheet2">
+    <Table ss:ExpandedColumnCount="1" ss:ExpandedRowCount="1" x:FullColumns="1"
+     x:FullRows="1">
+    </Table>
+    <WorksheetOptions xmlns="urn:schemas-microsoft-com:office:excel">
+     <ProtectObjects>False</ProtectObjects>
+     <ProtectScenarios>False</ProtectScenarios>
+    </WorksheetOptions>
+   </Worksheet>
+   <Worksheet ss:Name="Sheet3">
+    <Table ss:ExpandedColumnCount="1" ss:ExpandedRowCount="1" x:FullColumns="1"
+     x:FullRows="1">
+    </Table>
+    <WorksheetOptions xmlns="urn:schemas-microsoft-com:office:excel">
+     <ProtectObjects>False</ProtectObjects>
+     <ProtectScenarios>False</ProtectScenarios>
+    </WorksheetOptions>
+   </Worksheet>
+  </Workbook>
+';
   return $xml;
 }
 
