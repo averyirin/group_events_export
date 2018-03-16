c48ecc2ff6b4c00dd912f89407c127087cc64c0d
Irin <mpg.students+borden@gmail.com>
2018-03-16 13:54:24 -0400

    spreadsheet format

--- lib/group_events_export.php
+++ lib/group_events_export.php
@@ -114,14 +114,26 @@ function group_events_export_sheet($event){
   elgg_set_ignore_access(true);
   $EOL = "\r\n";
 
-  $worksheetXml .=  '<Row>
+  $worksheetXml =  '
+  <Row>
   <Cell><Data ss:Type="String">Molly</Data></Cell>
   <Cell><Data ss:Type="String">Polly</Data></Cell>
   <Cell><Data
   ss:Type="String">Katzen</Data></Cell>
   <Cell ss:StyleID="s21" ss:HRef="mailto:molly@katzen.com">
   <Data ss:Type="String">molly@katzen.com</Data></Cell>
-  </Row>';
+  </Row>
+  ';
+  $worksheetXml =  '
+  <Row>
+  <Cell><Data ss:Type="String">Molly</Data></Cell>
+  <Cell><Data ss:Type="String">Polly</Data></Cell>
+  <Cell><Data
+  ss:Type="String">Katzen</Data></Cell>
+  <Cell ss:StyleID="s21" ss:HRef="mailto:molly@katzen.com">
+  <Data ss:Type="String">molly@katzen.com</Data></Cell>
+  </Row>
+  ';
   $headerXml = '
    <Worksheet ss:Name="'.$event->title.'">
     <Names>
