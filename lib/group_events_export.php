3e009eea275bc21093ca2b660d0d24bd3d337053
Irin <mpg.students+borden@gmail.com>
2018-03-20 11:09:16 -0400

    desc table

--- lib/group_events_export.php
+++ lib/group_events_export.php
@@ -179,8 +179,7 @@ exit();
           $results = $xpath->query('/html/body/table/tr');
           foreach ($results as $result)
           {
-            $cells = $result -> getElementsByTagName('td');
-            echo var_dump($cells->item(0)->nodeValue)." , ".var_dump($cells->item(1)->nodeValue)."<br/>";
+            echo $result;
           }
 
 
