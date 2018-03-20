d95717af6aa1aeaeab2b04a01a1b3608d446f7a3
Irin <mpg.students+borden@gmail.com>
2018-03-20 11:05:03 -0400

    desc table

--- lib/group_events_export.php
+++ lib/group_events_export.php
@@ -176,6 +176,7 @@ exit();
         //  echo var_dump(htmlentities($data))."<br/>";
          echo $dom->documentElement;
 
+         echo $dom->saveHTML();
 /*
           $results = $xpath->query('table');
           foreach ($results as $result)
