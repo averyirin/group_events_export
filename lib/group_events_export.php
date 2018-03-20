cccbae5e4747ce0ecb0176751d17c41e7c8b4b60
Irin <mpg.students+borden@gmail.com>
2018-03-20 14:03:34 -0400

    add table to excel

--- lib/group_events_export.php
+++ lib/group_events_export.php
@@ -98,38 +98,47 @@ function group_events_export_overview($event){
       elgg_set_ignore_access($old_ia);
 
       if($peopleResponded) {
-        $dataXml .=  '<Row>
-        <Cell><Data ss:Type="String">'.(string)$event->title.'</Data></Cell>
-        <Cell><Data ss:Type="String">'.(string)$event->location.'</Data></Cell>
-        <Cell><Data ss:Type="String">'.(string)$event->venue.'</Data></Cell>
-        <Cell><Data ss:Type="String">'.(string)date("F d, Y h:i",$event->start_day).'</Data></Cell>
-        <Cell><Data ss:Type="String">'.(string)date("F d, Y h:i",$event->end_ts).'</Data></Cell>';
+        $includeEvent = true;
+        reset($peopleResponded);
+        foreach($peopleResponded as $attendee) {
+          $dataXml .=  '<Row>
+          <Cell><Data ss:Type="String">'.(string)$event->title.'</Data></Cell>
+          <Cell><Data ss:Type="String">'.(string)$event->location.'</Data></Cell>
+          <Cell><Data ss:Type="String">'.(string)$event->venue.'</Data></Cell>
+          <Cell><Data ss:Type="String">'.(string)date("F d, Y h:i",$event->start_day).'</Data></Cell>
+          <Cell><Data ss:Type="String">'.(string)date("F d, Y h:i",$event->end_ts).'</Data></Cell>';
 
 
 
-        $data = (string)($event->description);
-        $dom = new DOMDocument();
-        @$dom->loadHTML($data);
-        $dom->preserveWhiteSpace = false;
-        $xpath = new DOMXPath($dom);
+          $data = (string)($event->description);
+          $dom = new DOMDocument();
+          @$dom->loadHTML($data);
+          $dom->preserveWhiteSpace = false;
+          $xpath = new DOMXPath($dom);
 
-        $results = $xpath->query('/html/body/table/tbody/tr');
-        foreach ($results as $result){
-          $cells = $result -> getElementsByTagName('td');
-          $internalTables = $result -> getElementsByTagName('table');
-          $headerXml .=  '<Cell><Data ss:Type="String">'.(string)$cells->item(0)->nodeValue.'</Data></Cell>';
+          $results = $xpath->query('/html/body/table/tbody/tr');
+          foreach ($results as $result){
 
-        //    $dataXml .=  '<Cell><Data ss:Type="String">'.(string)$event->title.'</Data></Cell>';
-          //  echo ($cells->item(0)->nodeValue)." , ".($cells->item(1)->nodeValue)."<br/>";
-              foreach ($internalTables as $it) {
-                    $icells = $it -> getElementsByTagName('tr');
-                    foreach($icells as $val){
-                      $cells = $val -> getElementsByTagName('td');
-                    }
-              }
+            $cells = $result -> getElementsByTagName('td');
+            $internalTables = $result -> getElementsByTagName('table');
+            $dataXml .=  '<Cell><Data ss:Type="String">'.(string)$cells->item(0)->nodeValue.'</Data></Cell>';
+
+          //    $dataXml .=  '<Cell><Data ss:Type="String">'.(string)$event->title.'</Data></Cell>';
+            //  echo ($cells->item(0)->nodeValue)." , ".($cells->item(1)->nodeValue)."<br/>";
+                foreach ($internalTables as $it) {
+                      $icells = $it -> getElementsByTagName('tr');
+                      foreach($icells as $val){
+                        $cells = $val -> getElementsByTagName('td');
+                      }
+                }
+
+
+
+        //
+          }
+          $dataXml .= '</Row>';
+
         }
-        $dataXml .= '</Row>';
-
       }
   }
   $headerXml .= '</Row>';
