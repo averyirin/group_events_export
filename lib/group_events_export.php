c104977bb296dd4735e9037a838d9f10f506f921
Irin <mpg.students+borden@gmail.com>
2018-03-28 14:57:08 -0400

    formatting

--- lib/group_events_export.php
+++ lib/group_events_export.php
@@ -410,7 +410,7 @@ function group_events_export_sheet($event){
                  $descHeaderXml .=  '<Cell  ss:StyleID="s29"><Data ss:Type="String">'.($iDataCells->item(0)->nodeValue).'</Data></Cell>';
                  $descDataXml .='<Cell ss:StyleID="s30"><Data ss:Type="String">'.($iDataCells->item(1)->nodeValue).'</Data></Cell>';
                }
-               $descGeneralHeaderXml .= '<Cell ss:MergeAcross="'.($iColTotal-1).'" ss:StyleID="s29"><Data ss:Type="String">Description</Data></Cell>';
+               $descGeneralHeaderXml .= '<Cell ss:MergeAcross="'.($iColTotal-1).'" ss:StyleID="s31"><Data ss:Type="String">Description</Data></Cell>';
            }
           }else{
             $descColTotal++;
