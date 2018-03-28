a5016346e5e3484d7ec2cc6193a74a2daa881be6
Irin <mpg.students+borden@gmail.com>
2018-03-28 13:57:42 -0400

    formatting

--- lib/group_events_export.php
+++ lib/group_events_export.php
@@ -413,8 +413,6 @@ if($descTable != ""){
 
 //optional activity data table spacing
 $activityTable = $activityHeaderXml.$activityDataXml;
-echo var_dump($activityTable);
-exit();
 
 if($activityTable != ""){
      $activityTable .= $rowSpace;
