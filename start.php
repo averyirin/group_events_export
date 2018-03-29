71ca0190fdbafcec59c535d2ef8e027817d9d92e
Irin <mpg.students+borden@gmail.com>
2018-03-29 08:46:15 -0400

    formatting

--- start.php
+++ start.php
@@ -56,18 +56,20 @@ function group_events_export_pagesetup()
     $user = elgg_get_logged_in_user_entity();
     //add the group statistics button to the group events menu
     //! Does this need security?
-$uri = $_SERVER['REQUEST_URI'];
-
     if(elgg_in_context("events")&& ($page_owner instanceof ElggGroup)) {
       if((($who_create_group_events == "group_admin") && $page_owner->canEdit()) || (($who_create_group_events == "members") && $page_owner->isMember($user))){
 
-      elgg_register_menu_item('title', array(
-                'name' => "export",
-                'href' => "action/group_events_export/export?groupGuid=".$page_owner->getGuid(),
-                'text' => $uri,
-                'link_class' => 'elgg-button elgg-button-action',
-                'is_action' => true
-                ));
+        $uri = $_SERVER['REQUEST_URI'];
+        if (strpos($uri, '/event/list/') !== false) {
+          elgg_register_menu_item('title', array(
+                  'name' => "export",
+                  'href' => "action/group_events_export/export?groupGuid=".$page_owner->getGuid(),
+                  'text' => $uri,
+                  'link_class' => 'elgg-button elgg-button-action',
+                  'is_action' => true
+                  ));
+        }
+
     }
 
   }
